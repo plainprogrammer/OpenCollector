f25ca51d628a88789585e3956200e86dd01f805e
James Thompson <james@thomps.onl>
2024-03-27 19:31:53 -0400

    Refactor FetchMtgjsonSqlite implementation.

--- app/jobs/catalog/magic_the_gathering/fetch_mtgjson_sqlite.rb
+++ app/jobs/catalog/magic_the_gathering/fetch_mtgjson_sqlite.rb
@@ -1,5 +1,4 @@
 require "open-uri"
-require "tempfile"
 require "digest"
 
 module Catalog
@@ -10,19 +9,54 @@ module Catalog
       MTGJSON_SHA_URL = "https://mtgjson.com/api/v5/#{FILENAME}.sha256"
 
       def perform(since: 1.week.ago, directory: Rails.root.join("tmp/mtgjson"))
-        database_target = File.join(directory, FILENAME)
-        return if File.exist?(database_target) && File.mtime(database_target) >= since.to_time
+        @since = since
+        @directory = directory
 
-        database_tempfile = File.join(directory, "#{FILENAME}.tmp")
-        database = URI.open(MTGJSON_FILE_URL)
-        sha_hash = URI.open(MTGJSON_SHA_URL).read
+        return if more_recent_download_exists?
 
-        FileUtils.mkpath(directory)
-        IO.copy_stream(database, database_tempfile)
+        make_target_path
+        download_mtgjson_data
+        move_mtgjson_data_to_target
+      end
 
-        if Digest::SHA2.hexdigest(File.read(database_tempfile)) == sha_hash
-          FileUtils.mv(database_tempfile, database_target)
-        end
+      private
+
+      attr_reader :since, :directory
+
+      def database_target
+        File.join(@directory, FILENAME)
+      end
+
+      def database_tempfile
+        File.join(@directory, "#{FILENAME}.tmp")
+      end
+
+      def download_database
+        URI.open(MTGJSON_FILE_URL)
+      end
+
+      def download_matches_sha
+        Digest::SHA2.hexdigest(File.read(database_tempfile)) == sha_hash
+      end
+
+      def download_mtgjson_data
+        IO.copy_stream(download_database, database_tempfile)
+      end
+
+      def make_target_path
+        FileUtils.mkpath(@directory) unless File.exist?(database_target)
+      end
+
+      def more_recent_download_exists?
+        File.exist?(database_target) && File.mtime(database_target) >= @since.to_time
+      end
+
+      def move_mtgjson_data_to_target
+        FileUtils.mv(database_tempfile, database_target) if download_matches_sha
+      end
+
+      def sha_hash
+        URI.open(MTGJSON_SHA_URL).read
       end
     end
   end

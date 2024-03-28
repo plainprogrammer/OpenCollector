dbb8d8dfde4b6a80aa56dbf54856a205000f4fcd
James Thompson <james@thomps.onl>
2024-03-27 21:16:13 -0400

    Refactor specs for FetchMtgjsonSqlite job.

--- spec/jobs/catalog/magic_the_gathering/fetch_mtgjson_sqlite_spec.rb
+++ spec/jobs/catalog/magic_the_gathering/fetch_mtgjson_sqlite_spec.rb
@@ -3,12 +3,6 @@ require "rails_helper"
 RSpec.describe Catalog::MagicTheGathering::FetchMtgjsonSqlite do
   subject(:job) { described_class.new }
 
-  before do
-    if Rails.root.join("tmp/mtgjson/AllPrintings.sqlite.xz").exist?
-      Rails.root.join("tmp/mtgjson/AllPrintings.sqlite.xz").delete
-    end
-  end
-
   describe "MTGJSON Endpoint Checks" do
     let(:http_client) { Net::HTTP.new("mtgjson.com", 443) }
     let(:mtgjson_file_request) { http_client.head(URI(described_class::MTGJSON_FILE_URL).path) }
@@ -23,6 +17,17 @@ RSpec.describe Catalog::MagicTheGathering::FetchMtgjsonSqlite do
   end
 
   describe "#perform" do
+    let(:target_file) { Rails.root.join("tmp/mtgjson/AllPrintings.sqlite.xz") }
+    let(:backup_file) { "#{target_file}.bak" }
+
+    before do
+      FileUtils.mv(target_file, backup_file) if File.exist?(target_file)
+    end
+
+    after do
+      FileUtils.mv(backup_file, target_file) if File.exist?(backup_file)
+    end
+
     it "downloads the file" do
       expect {
         job.perform
@@ -34,22 +39,22 @@ RSpec.describe Catalog::MagicTheGathering::FetchMtgjsonSqlite do
     context "with :since argument" do
       context "and there is an existing file older than the :since argument" do
         before do
-          FileUtils.touch(Rails.root.join("tmp/mtgjson/AllPrintings.sqlite.xz"))
-          File.lutime(2.days.ago.to_time, 2.days.ago.to_time, Rails.root.join("tmp/mtgjson/AllPrintings.sqlite.xz"))
+          FileUtils.touch(target_file)
+          File.lutime(2.days.ago.to_time, 2.days.ago.to_time, target_file)
         end
 
         it "downloads the file" do
           expect {
             job.perform(since: 1.day.ago)
           }.to change {
-            Rails.root.join("tmp/mtgjson/AllPrintings.sqlite.xz").mtime
+            File.mtime(target_file)
           }
         end
       end
 
       context "and there is an existing file younger than the :since argument" do
         before do
-          FileUtils.touch(Rails.root.join("tmp/mtgjson/AllPrintings.sqlite.xz"))
+          FileUtils.touch(target_file)
           expect(URI).not_to receive(:open)
         end
 
